c74c543f4b25153455551e36944521332f398884
yiungyiung <yashparkhi.super@gmail.com>
2024-06-29 19:37:06 +0530

    Added  the Texture part and firebase auth and stuff

---
+++ Assets/Example/Scripts/DeviceButton.cs
@@ -0,0 +1,82 @@
+﻿using Android.BLE;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeviceButton : MonoBehaviour
+{
+    private string _deviceUuid = string.Empty;
+    private string _deviceName = string.Empty;
+
+    [SerializeField]
+    private Text _deviceUuidText;
+    [SerializeField]
+    private Text _deviceNameText;
+    [SerializeField]
+    private Text _deviceRssiText;
+
+    [SerializeField]
+    private Image _deviceButtonImage;
+    [SerializeField]
+    private Text _deviceButtonText;
+
+    [SerializeField]
+    private Color _onConnectedColor;
+    private Color _previousColor;
+
+    private bool _isConnected = false;
+
+    private BleDevice _bleDevice;
+
+    private float _rssiTimer = 0f;
+
+    public void Show(BleDevice device)
+    {
+        _deviceButtonText.text = "Connect";
+
+        _deviceUuid = device.MacAddress;
+        _deviceName = device.Name;
+
+        _deviceUuidText.text = device.MacAddress;
+        _deviceNameText.text = device.Name;
+
+        _bleDevice = device;
+    }
+
+    public void Update()
+    {
+        _rssiTimer += Time.deltaTime;
+        if (_rssiTimer > 0.5f)
+        {
+            _rssiTimer = 0f;
+            _bleDevice.GetRssi((_, rsi) => _deviceRssiText.text = rsi + " dBm");
+        }
+    }
+
+    public void Connect()
+    {
+        _bleDevice.Connect(OnConnected, OnDisconnected);
+    }
+
+    private void OnConnected(BleDevice device)
+    {
+        _previousColor = _deviceButtonImage.color;
+        _deviceButtonImage.color = _onConnectedColor;
+
+        _isConnected = true;
+        _deviceButtonText.text = "Disconnect";
+
+        device.GetCharacteristic("180C", "2A56").Subscribe((value) =>
+        {
+            Debug.Log(Encoding.UTF8.GetString(value));
+        });
+    }
+
+    private void OnDisconnected(BleDevice device)
+    {
+        _deviceButtonImage.color = _previousColor;
+
+        _isConnected = false;
+        _deviceButtonText.text = "Connect";
+    }
+}
---
+++ Assets/Example/Scripts/ExampleBleInteractor.cs
@@ -0,0 +1,27 @@
+﻿using Android.BLE;
+using UnityEngine;
+
+public class ExampleBleInteractor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _deviceButton;
+    [SerializeField]
+    private Transform _deviceList;
+
+    private bool _isScanning = false;
+
+    public void ScanForDevices()
+    {
+        if (!_isScanning)
+        {
+            _isScanning = true;
+            BleManager.Instance.SearchForDevices(10 * 1000, OnDeviceFound);
+        }
+    }
+
+    private void OnDeviceFound(BleDevice device)
+    {
+        DeviceButton button = Instantiate(_deviceButton, _deviceList).GetComponent<DeviceButton>();
+        button.Show(device);
+    }
+}
